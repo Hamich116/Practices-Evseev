14dbb63d6d23756052169f37264fe48244725dc6
Hamich <ihamatullin@gmail.com>
2020-11-29 17:22:29 +0300

    начало

---
+++ Custom List/Custom List/CustomList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Praktika6
+{
+    class CustomList<T> : IEnumerable<T>, ICloneable
+    {
+
+        private int capasity = 1;
+        int count = 0;
+        public int Count { get => count; }
+
+        public int Capasity { get => capasity; set => capasity=value; }
+        public int Length { get { return list.Length; } }
+
+        public CustomList() { }
+        public CustomList(IEnumerable<T> collection) { }
+        public CustomList(int capacity) { }
+
+        T[] list = new T[0];
+        public void Add(T item)
+        {
+            T[] newList = new T[list.Length + 1];
+            list.CopyTo(newList, 0);
+            newList[list.Length] = item;
+            list = newList;
+        }
+        
+        public void AddRange(IEnumerable<T> collection)
+        {
+            
+            T[] newList = new T[list.Length + 1];
+            list.CopyTo(newList, 0);
+            newList[list.Length] = (T)collection;
+            list = newList;
+            
+        }
+        
+        public void Clear()
+        {
+            list = new T[0];
+        }
+        
+        public CustomList<T> Clone()
+        {
+            
+        }
+
+        /*
+        public bool Contains(T item)
+        {
+
+        }
+        */
+        /*
+        public int IndexOf(T item, int index, int count)
+        {
+
+        }
+        */
+        /*
+        public int IndexOf(T item, int index)
+        {
+
+        }
+        */
+        /*
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] == item)
+                {
+                    return item;
+                }
+            }
+            return -1;
+        }
+        */
+        /*
+        public void Insert(int index, T item)
+        {
+
+        }
+        public void InsertRange(int index, IEnumerable<T> collection)
+        {
+
+        }
+        */
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(list, item);
+            if (index == -1) return;
+            T[] newList = new T[list.Length - 1];
+            int offset = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (i == index) { offset++; continue; }
+                newList[i - offset] = list[i];
+            }
+            list = newList;
+        }
+
+        /*
+        public int RemoveAll(Predicate<T> match)
+        {
+
+        }
+        public void Reverse(int index, int count)
+        {
+
+        }
+        public void Reverse()
+        {
+
+        }
+        */
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)list).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        object ICloneable.Clone()
+        {
+            return list.Clone();
+        }
+    }
+}
---
+++ Custom List/Custom List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Praktika6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> customList1 = new CustomList<int>() { 12, 578 };
+            CustomList<int> customList2 = new CustomList<int>() { 1, 2, 3, 4, 5 };
+            CustomList<int> customList3 = new CustomList<int>() { 132, 15, 45, 46 };
+            CustomList<int> customList4 = new CustomList<int>() { 187, 48, 78 };
+            /*
+            customList1.Add(15);
+            foreach (var item in customList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            customList2.AddRange(customList1); // ошибка
+
+            customList2.Remove(2);
+            foreach (var item in customList2)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            customList3.Clear();
+            foreach (var item in customList3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
