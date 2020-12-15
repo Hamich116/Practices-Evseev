4aebd203fd6741e19cdf26a1505cf912e9317747
Hamich <ihamatullin@gmail.com>
2020-12-15 15:54:50 +0300

    +SUPER HARD
    ну тупо считай изменил на Action делегат и считай лямбду сделал и все

---
+++ SUPER_HARD_PRAKTIK_BASED ON ASKAR_DELEGATES/TextBlockProj/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TextBlockProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBlock textBlock = new TextBlock("MyTextBlock", Color.Black, Font.Calibri);
+            textBlock.textEvent = (string oldText, string newText) =>
+            {
+                Console.WriteLine();
+                oldText = "";
+                for (int i = 0; i < newText.Length + 2; i++)
+                {
+                    oldText += "-";
+                }
+                oldText += $"\n|{newText}|\n";
+                for (int i = 0; i < newText.Length + 2; i++)
+                {
+                    oldText += "-";
+                }
+                Console.WriteLine(oldText);
+            };
+            while (true)
+            {
+                textBlock.Text += Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
---
+++ SUPER_HARD_PRAKTIK_BASED ON ASKAR_DELEGATES/TextBlockProj/TextBlock.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBlockProj
+{
+    public delegate void Action(string oldText, string newText);
+    public class TextBlock
+    {
+        string name;
+        string text;
+        Color textColor;
+        Font textFont;
+        public Action textEvent;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Text
+        {
+            get => text;
+            set
+            {
+                textEvent?.Invoke(text, value);
+                text = value;
+            }
+        }
+        public Color TextColor
+        {
+            get => textColor;
+            set => textColor = value;
+        }
+        public Font TextFont
+        { 
+            get => textFont;
+            set => textFont = value;
+        }
+
+        public TextBlock(string name, Color color, Font font)
+        {
+            this.name = name;
+            this.textColor = color;
+            this.textFont = font;
+        }
+    }
+}
