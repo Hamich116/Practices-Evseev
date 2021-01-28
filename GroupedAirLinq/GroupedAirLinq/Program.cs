f3bbe3bc797b6f41e05b3ff8c799417848fc4f9a
Hamich <ihamatullin@gmail.com>
2021-01-28 20:46:46 +0300

    6 сделано

--- GroupedAirLinq/GroupedAirLinq/Program.cs
+++ GroupedAirLinq/GroupedAirLinq/Program.cs
@@ -139,6 +139,75 @@ namespace GroupedAirLinq
                     }
                 }
             }
+            //Console.WriteLine("4.1");
+            //var allInfo = from flightcrew in FlightCrews
+            //              group flightcrew by flightcrew.FlightId;
+            //foreach (var item in allInfo)
+            //{
+            //    Console.WriteLine("Рейс: " + item.Key);
+            //    foreach (var item1 in item)
+            //    {
+            //        Console.WriteLine(item1.EmployeeId);
+            //    }
+            //}
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("4.2");
+            Console.WriteLine("5.1"); // ГОТОВО 
+            var countPalneCity = from planeCountry in Airports
+                                 group planeCountry by planeCountry.Country into c
+                                 select new
+                                 {
+                                     Id = c.Key,
+                                     Count = c.Count()
+                                 };
+            foreach (var item in countPalneCity)
+            {
+                Console.WriteLine($"Country - {item.Id} Count plane {item.Count}");
+            }
+
+            Console.WriteLine("5.2"); // ГОТОВО 
+
+            var countPalneCity1 = Airports.GroupBy(c => c.Country).Select(g => new
+            {
+                Id = g.Key,
+                Count = g.Count()
+            });
+            foreach (var item in countPalneCity1)
+            {
+                Console.WriteLine($"Country - {item.Id}, Count plane {item.Count}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("6.1");
+
+            var numberTicketsIsClients = from client in Clients
+                                         group client by client.Tickets.Count;
+            foreach (var item in numberTicketsIsClients)
+            {
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine($"Full name {item1.LastName} {item1.FirstName}, count {item.Key}, билеты:");
+                    foreach (var item2 in item1.Tickets)
+                    {
+                        Console.WriteLine(item2.Id);
+                    }
+                }
+            }
+            Console.WriteLine("6.2");
+            var numberTicketsIsClients1 = Clients.GroupBy(client => client.Tickets.Count);
+            foreach (var item in numberTicketsIsClients)
+            {
+                foreach (var item1 in item)
+                {
+                    Console.WriteLine($"Full name {item1.LastName} {item1.FirstName}, count {item.Key}, билеты:");
+                    foreach (var item2 in item1.Tickets)
+                    {
+                        Console.WriteLine(item2.Id);
+                    }
+                }
+            }
         }
     }
 }
